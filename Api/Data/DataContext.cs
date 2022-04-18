a06d1045d7d5d2005177a8b43a21cfdb584532ea
IrfanMonir <uddin@s3innovate.com>
2022-04-18 20:30:44 +0600

    added DbContext and created connection string

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Api.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,15 @@
+using Api.Data;
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
