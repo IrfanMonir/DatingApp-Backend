bec6bb164818eecb126df0322a7e74d8fd92ed8a
IrfanMonir <uddin@s3innovate.com>
2022-04-18 21:24:03 +0600

    added new api controller

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Api.Data;
+using Api.Entities;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers() => _context.Users.ToList();
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id) => _context.Users.Find(id);
+    }
+}
